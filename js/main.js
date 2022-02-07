3d353d0d8e02d6d100bf0e544f958ef8b9f91cf0
Roman <ramzez1@gmail.com>
2022-02-07 19:15:25 +0300

    07022022_1915

--- js/main.js
+++ js/main.js
@@ -1,29 +1,87 @@
-$(function() {
-    $(".selectlink-js").on('click', function(e){
-        // $('.Flag__dropdown').slideToggle();
+$(function () {
+    //прилипает меню к верху
+    $(window).scroll(function () {
+        var wScrTop = $(window).scrollTop()
+        if (wScrTop > 0) {
+            $('.Top').addClass('Fixed-bar')
+        } else {
+            $('.Top').removeClass('Fixed-bar')
+        }
 
+    });
+
+    //select для языков
+    $(".selectlink-js").on('click', function (e) {
         var $menu_popup = $(this).next();
-        $menu_popup.slideToggle(200, function(){
-
+        $menu_popup.slideToggle(200, function () {
             $('.Flag .Flag__dropdown').not($menu_popup).slideUp(200);
-            
             if ($menu_popup.is(':hidden')) {
                 $('.Flag__dropdown').removeClass('active');
                 $(".selectlink-js").removeClass('active');
             } else {
-                  $('.Flag__dropdown').addClass('active');
-                  $(".selectlink-js").addClass('active');
-            }					
-        });  
+                $('.Flag__dropdown').addClass('active');
+                $(".selectlink-js").addClass('active');
+            }
+        });
         return false;
-    });		
-    $('.Flag-js').on('click', function(e){
-        if($('.Flag__dropdown').hasClass('active')){
+    });
+    $('.Flag-js').on('click', function (e) {
+        if ($('.Flag__dropdown').hasClass('active')) {
             $(".selectlink-js").html($(this).html());
             $('.Flag .Flag__dropdown').slideUp(200);
             $('.Flag .Flag__dropdown').removeClass('active');
             $(".selectlink-js").removeClass('active');
         }
+    });
+
+    //подключение слайдера для отзывово
+    $('.Reviews-slider').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        items: 4,
+        dots: false,
+        navClass: ['slick-prev-t', 'slick-next-t'],
+        responsiveClass: true,
+        responsive: {
+            320: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            }
+
+        }
+
+    });
+    //работа бургера меню
+    $('#hamburger').click(function (e) {
+        $('.dropDownMenu').fadeToggle();
+        $(this).toggleClass('active');
+        $('.dropDownMenu').toggleClass('active');
         
     });
+    //закрыть боковое меню
+    $('#slide-close').click(function (e) {
+        $('.dropDownMenu').fadeToggle();
+        $('#hamburger').toggleClass('active');
+        $('.dropDownMenu').toggleClass('active');
+        
+    });
+
+    //подключение модалки
+    $('.bpopup').click(function(e) {
+        e.preventDefault();
+        var href = $(this).attr('href');
+        $(href).bPopup({
+                closeClass:'close'
+            });
+    });
+
 });
